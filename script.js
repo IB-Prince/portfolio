d8efbca7ae305b9a48926b697c4c7f1a568f2879
IB Prince <ibprince48@gmail.com>
2024-05-25 20:01:45 +0600

    Updated script.js
    My roles are updated.

--- script.js
+++ script.js
@@ -25,14 +25,14 @@ $(document).ready(function(){
 
     // typing animation script
     var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["CSE Student", "Flutter Mobile App Developer", "Python Data Analytics", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["CSE Student", "Flutter Mobile App Developer", "Python Data Analytics", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
